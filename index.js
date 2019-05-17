09af0e7b3e7f24aa3241f417988d64095637b134
chachangthong <49773794+chachangthong@users.noreply.github.com>
2019-05-17 10:39:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -97,148 +97,6 @@ console.log(printdd)
         if(body>= 10){
 var ppoint  = body;
 console.log("ppoint : "+ppoint)
-return res.json({
-    "fulfillmentMessages": [
-  {
-    "platform": "line",
-    "type": 4,
-    "payload" : {
-    "line":  
-    ////////////////////////////////////////////////////////////////////////////////////////
-    {
-  "type": "flex",
-  "altText": "คะแนนสะสม",
-  "contents": {
-    "type": "bubble",
-    "direction": "ltr",
-    "body": {
-      "type": "box",
-      "layout": "horizontal",
-      "contents": [
-        {
-          "type": "box",
-          "layout": "vertical",
-          "flex": 2,
-          "contents": [
-            {
-              "type": "image",
-              "url": pictureUrl,
-              "align": "start",
-              "gravity": "top",
-              "size": "sm",
-              "aspectRatio": "1:1"
-            }
-          ]
-        },
-        {
-          "type": "box",
-          "layout": "vertical",
-          "flex": 4,
-          "spacing": "none",
-          "contents": [
-            {
-              "type": "box",
-              "layout": "baseline",
-              "spacing": "md",
-              "contents": [
-                {
-                  "type": "text",
-                  "text": "  สวัสดีค่ะ",
-                  "flex": 0,
-                  "size": "md",
-                  "align": "start",
-                  "color": "#000000"
-                },
-                {
-                  "type": "text",
-                  "text": name,
-                  "flex": 0,
-                  "size": "md",
-                  "weight": "bold",
-                  "color": "#0E09BF"
-                }
-              ]
-            },
-            {
-              "type": "box",
-              "layout": "vertical",
-              "contents": [
-                {
-                  "type": "text",
-                  "text": "คุณมียอดคะแนนสะสม",
-                  "flex": 0,
-                  "size": "md",
-                  "align": "center",
-                  "gravity": "center",
-                  "color": "#000000"
-                }
-              ]
-            },
-            {
-              "type": "box",
-              "layout": "vertical",
-              "contents": [
-                {
-                  "type": "text",
-                  "text": ppoint,//////////////////////////////
-                  "flex": 0,
-                  "size": "xl",
-                  "align": "center",
-                  "weight": "bold",
-                  "color": "#FF0000"
-                }
-              ]
-            }
-          ]
-        }
-      ]
-    },
-    "footer": {
-      "type": "box",
-      "layout": "vertical",
-      "spacing": "none",
-      "margin": "none",
-      "contents": [
-        {
-          "type": "image",
-          "url": "https://www.igetweb.com/uploads/269/filemanager/a6e6cbcc70f574bd9683dc2d998e08e4_full.jpg",
-          "margin": "xl",
-          "align": "center",
-          "gravity": "center",
-          "size": "full",
-          "aspectRatio": "16:9",
-          "aspectMode": "cover"
-        },
-        {
-          "type": "button",
-          "action": {
-            "type": "uri",
-            "label": "ดูเพิ่มเติม",
-            "uri": "line://app/1566203051-GMalZd6Q"
-          },
-          "margin": "md",
-          "height": "sm",
-          "style": "primary",
-          "gravity": "bottom"
-        }
-      ]
-    }
-  }
-}        
- ////////////////////////////////////////////////////// ////////////////////////////////        
-        
-    
-}
-}
-]
-});          
-          
-          
-          
-        }else if (body == "Id not found" || body <= 9 ){
-        // ไม่มีข่อมูล ส่งข้อมูล user ไปเก็บ /// ตอบกลับ ติดต่อเจ้าหน้าที่
-        var sendName = encodeURI(name);       
-      request("https://docs.google.com/forms/u/2/d/1iUGX58guFhU3bkt1OglhOGoDuv5i6mPQAs35gy4IOcw/formResponse?ifq&entry.1691916586="+iiddd+"&entry.556749397="+sendName+"&entry.1687867422="+pictureUrl+"&entry.66040433=0&entry.1800492209=data2&entry.53513319=data3&entry.1987831678=data4&submit=Submit");
 
 return res.json({
     "fulfillmentMessages": [
@@ -246,144 +104,39 @@ return res.json({
     "platform": "line",
     "type": 4,
     "payload" : {
-    "line":  
-    /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-{
-  "type": "flex",
-  "altText": "คะแนนสะสม",
-  "contents": {
-    "type": "bubble",
-    "direction": "ltr",
-    "body": {
-      "type": "box",
-      "layout": "horizontal",
-      "contents": [
-        {
-          "type": "box",
-          "layout": "vertical",
-          "flex": 2,
-          "contents": [
-            {
-              "type": "image",
-              "url": pictureUrl,
-              "align": "start",
-              "gravity": "top",
-              "size": "sm",
-              "aspectRatio": "1:1"
-            }
-          ]
-        },
-        {
-          "type": "box",
-          "layout": "vertical",
-          "flex": 4,
-          "spacing": "none",
-          "contents": [
-            {
-              "type": "box",
-              "layout": "baseline",
-              "spacing": "md",
-              "contents": [
-                {
-                  "type": "text",
-                  "text": "  สวัสดีค่ะ",
-                  "flex": 0,
-                  "size": "md",
-                  "align": "start",
-                  "color": "#000000"
-                },
-                {
-                  "type": "text",
-                  "text": name,
-                  "flex": 0,
-                  "size": "md",
-                  "weight": "bold",
-                  "color": "#0E09BF"
-                }
-              ]
-            },
-            {
-              "type": "box",
-              "layout": "vertical",
-              "contents": [
-                {
-                  "type": "text",
-                  "text": "คุณมียอดคะแนนสะสม",
-                  "flex": 0,
-                  "size": "md",
-                  "align": "center",
-                  "gravity": "center",
-                  "color": "#000000"
-                }
-              ]
-            },
-            {
-              "type": "box",
-              "layout": "vertical",
-              "contents": [
-                {
-                  "type": "text",
-                  "text": "ติดต่อเจ้าหน้าที่",
-                  "flex": 0,
-                  "size": "xs",
-                  "align": "center",
-                  "weight": "bold",
-                  "color": "#FF0000"
-                },
-                {
-                  "type": "text",
-                  "text": "เพื่อขออัพเดทคะแนนสะสม",
-                  "flex": 0,
-                  "size": "xs",
-                  "align": "center",
-                  "weight": "bold",
-                  "color": "#FF0000"
-                }
-              ]
-            }
-          ]
-        }
-      ]
-    },
-    "footer": {
-      "type": "box",
-      "layout": "vertical",
-      "spacing": "none",
-      "margin": "none",
-      "contents": [
-        {
-          "type": "image",
-          "url": "https://www.igetweb.com/uploads/269/filemanager/a6e6cbcc70f574bd9683dc2d998e08e4_full.jpg",
-          "margin": "xl",
-          "align": "center",
-          "gravity": "center",
-          "size": "full",
-          "aspectRatio": "16:9",
-          "aspectMode": "cover"
-        },
-        {
-          "type": "button",
-          "action": {
-            "type": "uri",
-            "label": "ดูเพิ่มเติม.",
-            "uri": "line://app/1566203051-GMalZd6Q"
-          },
-          "margin": "md",
-          "height": "sm",
-          "style": "primary",
-          "gravity": "bottom"
-        }
-      ]
-    }
-  }
+    "line":  {
+  "type": "text",
+  "text": ppoint
 }
- ////////////////////////////////////////////////////// //////////////////////////////////////////////////////////////////////////////////////////        
         
-    
+    }
+    }
+      ]
+  })
+            
+            
+          
+          
+          
+        }else if (body == "Id not found" || body <= 9 ){
+        // ไม่มีข่อมูล ส่งข้อมูล user ไปเก็บ /// ตอบกลับ ติดต่อเจ้าหน้าที่
+        var sendName = encodeURI(name);       
+      request("https://docs.google.com/forms/u/2/d/1iUGX58guFhU3bkt1OglhOGoDuv5i6mPQAs35gy4IOcw/formResponse?ifq&entry.1691916586="+iiddd+"&entry.556749397="+sendName+"&entry.1687867422="+pictureUrl+"&entry.66040433=0&entry.1800492209=data2&entry.53513319=data3&entry.1987831678=data4&submit=Submit");
+return res.json({
+    "fulfillmentMessages": [
+  {
+    "platform": "line",
+    "type": 4,
+    "payload" : {
+    "line":  {
+  "type": "text",
+  "text": "Hello"
 }
-}
-]
-});
+        
+    }
+    }
+      ]
+  })
 
 
 
